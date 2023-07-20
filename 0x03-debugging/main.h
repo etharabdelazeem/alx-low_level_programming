9b21ac3611251dd93688c93d9a5ad51ed2d56ac2
Ethar <etharabdelazeem1@gmail.com>
2023-07-20 22:06:49 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,32 @@
 
 void positive_or_negative(int i);
 
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b && a > c)
+    {
+        largest = a;
+    }
+    else if (b > a && b > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
+
 #endif /* MAIN_H */
