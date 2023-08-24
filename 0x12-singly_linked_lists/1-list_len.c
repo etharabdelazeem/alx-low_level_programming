d61a9af5a8b78c737b92977c1381fa4fa2373b89
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-08-25 01:01:09 +0200

    fix betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,12 +7,12 @@
 */
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while(h != NULL)
-    {
-        h = h->next;
-        count++;
-    }
-    return(count);
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
