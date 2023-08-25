51aec6553b679037123c5fdd789d0a38e13293b0
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-08-24 17:00:04 -0700

    betty (:

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	size_t length;
-	
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
@@ -19,7 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	length = strlen(str);
 	new_node->len = length;
-	new_node->next= *head;
+	new_node->next = *head;
 	*head = new_node;
 	return (*head);
 }
