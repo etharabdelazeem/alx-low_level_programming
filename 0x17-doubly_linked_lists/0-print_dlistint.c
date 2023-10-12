7cfbcd49d67639c4b8ef59bdf0e2aa1cacf90743
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-12 22:25:00 +0000

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of a dlistint_t list
+ * @h: list head
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counter = 0;
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n",h->n);
+		counter++;
+		h = h->next;
+	}
+	return counter;
+}
