a4157662f767d4880c947316c7992532e71b9f0b
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-05-07 19:48:33 +0000

    betty fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,13 +6,13 @@
  * @size: The number of elements in array.
  * @value: The value to search for.
  * Return: The first index where value is located.
- * 		or -1 If value is not present in array or if array is NULL.
+ *		or -1 If value is not present in array or if array is NULL.
  */
 int linear_search(int *array, size_t size, int value)
 {
-	long unsigned int i=0;
+	unsigned long int i = 0;
 
-	while(array && i<size)
+	while (array && i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, *array);
 		if (*array == value)
